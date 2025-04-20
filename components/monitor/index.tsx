295445b7d34daeaffbf645e042b91c09adc521bd
zard0520 <422451702@qq.com>
2025-04-21 00:23:13 +0800

    update

--- components/monitor/index.tsx
+++ components/monitor/index.tsx
@@ -2,10 +2,12 @@
 
 import createMonitor from '../../lib/monitor';
 import React, { useEffect } from 'react';
+import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/router';
 
 export default function ClientSideMonitor() {
   useEffect(() => {
-    createMonitor(React, { useHistory, useLocation })
+    createMonitor(React, { useRouter, usePathname }, {}, {})
     // const m = new Monitor()
     // return () => m.destory()
   })
