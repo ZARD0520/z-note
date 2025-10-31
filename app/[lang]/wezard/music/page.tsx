e5b9752f401324fdd1747f9faf911da175b3c8a4
huangjunjie <huangjunjie@newlang.cn>
2025-10-31 18:31:08 +0800

    feat: 暂存

--- app/[lang]/wezard/music/page.tsx
+++ app/[lang]/wezard/music/page.tsx
@@ -3,17 +3,28 @@ import { Locale } from "@/i18n/config"
 import AlbumGrid from './component/album'
 import { FloatingBackButton } from "@/components/wezard/back"
 
+async function getFirstPageData() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}?type=music&current=1&limit=10`, {
+    next: { revalidate: 300 } // 5分钟重新验证
+  })
+  if (!res.ok) {
+    throw new Error('Failed to fetch products');
+  }
+  return res.json();
+}
+
 export default async function WezardAlbums({ params: { lang } }: {
   params: {
     lang: Locale
   }
 }) {
   const dict = await getDictionary(lang)
+  const firstPageData = await getFirstPageData()
 
   return (
     <div className="h-screen overflow-auto bg-gradient-to-br from-gray-800 via-slate-800 to-gray-900 text-white">
       <FloatingBackButton />
-      <AlbumGrid dict={dict}/>
+      <AlbumGrid dict={dict} initialData={firstPageData} initialPage={1}/>
     </div>
   )
 }
